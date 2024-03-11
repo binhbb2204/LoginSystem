18872d563bd630f59a398ca3fa500eed2fa495cb
binhbb2204 <153449725+binhbb2204@users.noreply.github.com>
2024-03-11 22:37:09 +0700

    Add files via upload

---
+++ LoginAndSignUp/src/loginandsignup/LoginAndSignUp.java
@@ -0,0 +1,15 @@
+
+package loginandsignup;
+
+public class LoginAndSignUp {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Hello");
+        Login LoginFrame = new Login();
+        LoginFrame.setVisible(true);
+        LoginFrame.pack();
+        LoginFrame.setLocationRelativeTo(null);
+    }
+    
+}
